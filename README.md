3e4f1eb79e00bcfff7c3feb1d8ed99dd7d62921d
Juho <Juho9179@gmail.com>
2021-03-18 15:30:01 +0200

    clojure support
    clojure support

--- README.md
+++ README.md
@@ -1,2 +1,75 @@
-# sbom-machine
-Reads recursively package.json and other manifest files for dependencies and versions
+# SBOM Script
+Reads recursively manifest files in a repository, exports dependencies and dependency versions in a readable format
+
+# What is SBOM? (Software bill of materials)
+https://en.wikipedia.org/wiki/Software_bill_of_materials
+
+# Usage
+python3 sbom.py <target repository> <export name> params
+
+Params:
+-h or --help          displays help
+-f or --force         forces overwriting of export file
+-v or --verbose       verbose output
+-a or --append        Appends to file
+
+Example:          
+
+python3 .\sbom.py project-repo export.txt -v -f                           
+
+Then appending other project to the same export file
+
+python3 .\sbom.py another-project export.txt -v -f -a 
+
+# Current features
+Reads package.json and deps.edn files recursively from a directory
+
+Supports both, windows and linux-based environments
+
+Example export.txt:
+
+Software Bill of Materials (SBOM)
+18/03/2021 14:53:46
+
+#################
+
+Component name: @project/a-package-json-example
+
+Dependencies:
+date-fns: ^2.12.0
+dayjs: ^1.9.6
+sanitize-filename: ^1.6.3
+uuid: ^8.3.1
+
+Development dependencies:
+jest: ^26.6.0
+
+#################
+
+Component name: .\clojuretest\deps.edn
+
+Dependencies:
+org.clojure/clojure: mvn/version 1.10.1
+integrant/repl: mvn/version 0.3.1
+org.clojure/clojurescript: mvn/version 1.10.597
+reagent/reagent: mvn/version 0.10.0
+binaryage/devtools: mvn/version 1.0.0
+syn-antd/syn-antd: mvn/version 4.0.0-rc.1
+integrant/integrant: mvn/version 0.8.0
+clj-http/clj-http: mvn/version 3.10.1
+hikari-cp/hikari-cp: mvn/version 2.11.0
+com.layerware/hugsql: mvn/version 0.5.1
+org.postgresql/postgresql: mvn/version 42.2.12
+org.clojure/java.jdbc: mvn/version 0.7.11
+org.flywaydb/flyway-core: mvn/version 6.3.3
+org.clojure/tools.logging: mvn/version 1.0.0
+ch.qos.logback/logback-classic: mvn/version 1.3.0-alpha5
+com.fasterxml.jackson.datatype/jackson-datatype-joda: mvn/version 2.10.0
+lambdaisland/kaocha: mvn/version 1.0-612
+lambdaisland/kaocha-cljs: mvn/version 0.0-71
+lambdaisland/kaocha-cloverage: mvn/version 1.0-45
+ch.qos.logback/logback-classic: mvn/version 1.3.0-alpha5
+
+Development dependencies:
+
+#################
--- sbom.py
+++ sbom.py
@@ -1,33 +1,57 @@
 # Author: Juho Lappalainen
-author = "Juho9179@gmail.com"
-version = "0.1"
 # Recursively extracts dependencies and version numbers from a repository manifest files, such as package.json
 # Exports found dependencies in a readable format
+# 
+# Supports: package.json and deps.edn manifest files
 #
 # Usage:
 # python3 sbom.py <target repository> <export name> params
 # Params:
+# -h or --help          displays help
 # -f or --force         forces overwriting of export file
 # -v or --verbose       verbose output
+# -a or --append        Appends to file
 
 import os
+import re
 import glob
 import platform
 import json
 import sys
 from datetime import datetime
 
+settings = {}
+settings["author"] = "Juho9179@gmail.com"
+settings["version"] = "0.1"
+
 def verbose_print(string):
     global settings
     if (not settings["quiet"]):
         print(string)
 
+def print_header():
+    global settings
+    print("")
+    print("\t###################################")
+    print("\t##   SBOM Extract                ##")
+    print("\t##   Author: " + settings["author"] + "\t ##")
+    print("\t##   Version: v" + settings["version"] + "\t\t ##")
+    print("\t###################################")
+
 def print_usage():
-    print("## SBOM ##")
-    print("Usage:")
-    print(sys.argv[0] + " <target repository> <export file> [parameters]")
+    print_header()
+    print("")
+    print("\nUsage:\n")
+    print(sys.argv[0] + " <target repository> <export file> [parameters]\n")
     print("Parameters:")
-    print("-f or --force\tforces overwrite of export file (default: appends)")
+    print("\t-h or --help\tdisplays help")
+    print("\t-f or --force\tforces overwrite of export file - doesn't ask for input if export file exists")
+    print("\t-a or --append\tappends output to export file, instead of overwriting it.")
+    print("\t-v or --verbose\tverbose output\n")
+    print("Example:\n")
+    print("\t" + sys.argv[0] + " project-repo export.txt -v -f\n")
+    print("Then appending other project to the same export file\n")
+    print("\t" +sys.argv[0] + " another-project export.txt -v -f -a\n")
 
 def get_delimeter():
     # Check OS, set delimiter
@@ -81,50 +105,63 @@ def process_package_json(filepath):
 
 def process_deps_edn(filepath):
     # Processes deps.edn file and returns object
-    # TODO: Implement
-    verbose_print("[-] deps.edn processing not implemented")
-
-    # Processes package.json in given filepath
-    #f = open(filepath)
-    #data = json.load(f)
-
-    ## get name, if no name, use file path
-    #name = ""
-    #try:
-    #    name = data["name"]
-    #except KeyError:
-    #    name = filepath
-
-    #devdeps = []
-    #deps = []
-    #try:
-    #    for i in data["devDependencies"]:
-    #        dep = {}
-    #        dep["name"] = i
-    #        dep["version"] = data["devDependencies"][i]
-    #        devdeps.append(dep)
-    #except KeyError:
-    #    verbose_print("[-] No development dependencies for " + name)
-
-
-    #try:
-    #    for i in data["dependencies"]:
-    #        dep = {}
-    #        dep["name"] = i
-    #        dep["version"] = data["dependencies"][i]
-    #        deps.append(dep)
-    #except KeyError:
-    #    verbose_print("[-] No dependencies for " + name)
-
-    #f.close()
-    
-    # create object to be returned
+    # Parse the edn file line by line
+    # Process accordingly and return object
+    f = open(filepath)
+    contents = f.readlines()
+    ignorelist = [":","{", "[", ";", '"', "'"]
+    exceptionlist = [":extra-deps", ":deps"]
+    deps = []
+    for line in contents:
+        try:
+            if ((line.strip()[0] not in ignorelist) or (line.strip().split(" ")[0] in exceptionlist)):
+                if (line.strip().split(" ")[0] in exceptionlist):
+                    # special case
+                    deps.append(parse_edn_first_dep(line.strip()))
+                else:
+                    # regular case
+                    deps.append(parse_edn_dep(line.strip()))
+        except IndexError:
+            if (line != "\n"):
+                verbose_print("[-] Issue processing line with content: " + line.strip())
     exported = {}
-    exported["name"] = name
+    exported["name"] = filepath
     exported["dependencies"] = {}
-    exported["dependencies"]["development_dependencies"] = devdeps
+    exported["dependencies"]["development_dependencies"] = []
     exported["dependencies"]["dependencies"] = deps
     return exported
+
+def parse_edn_first_dep(dep):
+    # Parse special case, where dependency is on the same line with declaration
+    # return object { name: name, version: version }
+    rest = dep.strip().split(" ")[1:]
+    version = re.findall(r'"(.*?)"', rest[2])
+    dep = {}
+    dep["name"] = rest[0][1:]
+    try:
+        dep["version"] = rest[1][2:] + " " + version[0]
+    except IndexError:
+        verbose_print("[-] IndexError handling: " + dep)
+        dep["version"] = rest[1][2:] + " " + rest[2]
+    return dep
+
+
+def parse_edn_dep(dep):
+    # parse edn dep, return object { name: name, version: version }
+    entry = dep.split(" ")
+    entry_name = entry[0]
+    entry_version = entry[1:]
+    entry_source = entry_version[0][2:]
+
+    version = re.findall(r'"(.*?)"', entry_version[1])
+    dep = {}
+    dep["name"] = entry_name
+    try:
+        dep["version"] = entry_source + " " + version[0]
+    except IndexError:
+        dep["version"] = entry_source + " " + entry_version[1]
+
+    return dep
 
 def process_manifest(filepath):
     # Determines manifest type and processes accordingly
@@ -157,7 +194,7 @@ def process_manifest(filepath):
 def find_manifests(targetrepo):
     # Find all manifest files
     # Currently supports:
-    # package.json
+    # package.json, deps.edn
     manifests_types = ["package.json", "deps.edn"]
     manifest_files = []
     
@@ -210,6 +247,8 @@ def overwrite_check(targetfile):
                 return True
             else:
                 return False
+    elif (settings["append"] == True):
+        return False
     else:
         return True
     
@@ -241,22 +280,23 @@ def clean_target(targetfile):
 def init_settings():
     # Initialize global settings
     # Check for missing arguments
-    global version
-    global author
-    if (len(sys.argv) < 3):
+    global settings
+    if ((len(sys.argv) < 3) or ("-h" in sys.argv) or ("--help" in sys.argv)):
         print_usage()
         exit()
 
     # Initialize settings
-    settings = {}
-    settings["version"] = version
-    settings["author"] = author
     settings["target"] = sys.argv[1]
     settings["export"] = sys.argv[2]
     if (("-f" in sys.argv) or ("--force" in sys.argv)):
         settings["force"] = True
     else:
         settings["force"] = False
+
+    if (("-a" in sys.argv) or ("--append" in sys.argv)):
+        settings["append"] = True
+    else:
+        settings["append"] = None
 
     if (("-v" in sys.argv) or ("--verbose" in sys.argv)):
         settings["quiet"] = False
@@ -266,13 +306,9 @@ def init_settings():
 
 # main function
 def main():
-    global settings
     settings = init_settings()
-    verbose_print("###################################")
-    verbose_print("##   SBOM Extract                ##")
-    verbose_print("##   Author: " + settings["author"] + "\t ##")
-    verbose_print("##   Version: v" + settings["version"] + "\t\t ##")
-    verbose_print("###################################")
+    if (not settings["quiet"]):
+        print_header()
     verbose_print("[i] Target repository: " + settings["target"])
     verbose_print("[i] Export file: " + settings["export"])
     verbose_print("[i] Force: " + str(settings["force"]))
